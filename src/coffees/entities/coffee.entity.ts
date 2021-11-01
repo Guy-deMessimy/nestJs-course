9dc9f16c386b7edf2ba80f06afa9730680dda6ba
Guy de Messimy <gdemessimy@gmail.com>
2021-11-01 21:19:55 +0100

    add and configure typeorm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() //sql table === 'coffees' otherwise we can precise an entity string parameter
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  // use the 2 parameter to store array's as json and make column optionnal
+  @Column('json', { nullable: true })
   flavors: string[];
 }
